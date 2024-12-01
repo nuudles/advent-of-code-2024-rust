0eb5271eb97edf8192b0d1f6b7e596454b39a3d9
Christopher Luu <christopherluu@netflix.com>
2024-12-01 07:42:10 -0500

    Add Day 1 solution

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub type DayFn = fn(String);
 
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     match day {
-        1 => (day01::part1, noop),
+        1 => (day01::part1, day01::part2),
         _ => {
             println!("Unknown day: {}", day);
             (noop, noop)
