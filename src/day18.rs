011548cef3c0aa95cd9d9cab6a59ca3302f35a75
Christopher Luu <nuudles@gmail.com>
2024-12-18 15:53:30 -0500

    Add visualization for Day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,9 +1,54 @@
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    io::{self, stdout, Write},
+};
 
+use crossterm::{
+    cursor, queue,
+    style::{PrintStyledContent, Stylize},
+    terminal::{Clear, ClearType},
+    ExecutableCommand,
+};
 use itertools::Itertools;
 use pathfinding::prelude::astar;
 
 use crate::{parse_nums::parse_nums, point::Point, selfprint::SelfPrint};
+
+fn visualize(
+    width: i64,
+    height: i64,
+    corrupted: &HashSet<Point<i64>>,
+    best_path: &HashSet<Point<i64>>,
+) -> io::Result<()> {
+    let mut stdout = stdout();
+
+    for y in 0..height {
+        for x in 0..width {
+            let p = Point { x, y };
+            if corrupted.contains(&p) {
+                queue!(
+                    stdout,
+                    cursor::MoveTo(x as u16, y as u16),
+                    PrintStyledContent("█".magenta())
+                )?;
+            } else if best_path.contains(&p) {
+                queue!(
+                    stdout,
+                    cursor::MoveTo(x as u16, y as u16),
+                    PrintStyledContent("█".green())
+                )?;
+            } else {
+                queue!(
+                    stdout,
+                    cursor::MoveTo(x as u16, y as u16),
+                    PrintStyledContent("█".black())
+                )?;
+            }
+        }
+    }
+    stdout.flush()?;
+    Ok(())
+}
 
 pub fn part1(input: String) {
     // let (width, height) = (7i64, 7i64);
@@ -36,6 +81,8 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
+    _ = stdout().execute(Clear(ClearType::All));
+
     // let (width, height) = (7i64, 7i64);
     let (width, height) = (71i64, 71i64);
     let mut corrupted = HashSet::new();
@@ -47,6 +94,8 @@ pub fn part2(input: String) {
     for (x, y) in parse_nums::<i64>(&input).tuples() {
         let p = Point { x, y };
         corrupted.insert(p);
+
+        _ = visualize(width, height, &corrupted, &best_path);
 
         if best_path.len() == 0 || best_path.contains(&p) {
             if let Some(path) = astar(
@@ -69,6 +118,7 @@ pub fn part2(input: String) {
             ) {
                 best_path = path.0.iter().cloned().collect();
             } else {
+                println!();
                 println!("{},{}", x, y);
                 break;
             }
